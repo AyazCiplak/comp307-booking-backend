ca7ec1d24c395078e1df41d97a8ad0082031afc4
Ayaz Ciplak <ayazciplak@gmail.com>
2026-04-28 19:01:41 -0400

    reverted changes

--- src/main/java/comp307/backend/booking/Repository/GroupMeetingInstanceRepository.java
+++ src/main/java/comp307/backend/booking/Repository/GroupMeetingInstanceRepository.java
@@ -2,13 +2,19 @@
 
 package comp307.backend.booking.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import comp307.backend.account.Object.User;
 import comp307.backend.booking.Entity.GroupMeetingInstance;
 
 @Repository
 public interface GroupMeetingInstanceRepository extends JpaRepository<GroupMeetingInstance, Long> {
-    Optional<GroupMeetingInstance> findByInviteToken(String inviteToken);
+    /** Returns all group meeting instances owned by a given user. */
+    List<GroupMeetingInstance> findByOwner(User owner);
+
+    /** Returns instances with the given invite token (should always be exactly 1 due to unique constraint). */
+    List<GroupMeetingInstance> findByInviteToken(String inviteToken);
 }
